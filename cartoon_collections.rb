aba2ca22e90f7fd8268533c2a5981c8195e9f96c
flored27 <dflores@fandm.edu>
2017-09-27 14:34:37 -0400

    interesting

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.collect {|dwarf| puts "#{array.index(dwarf) + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|x| x[0] = x[0].capitalize}
+  array.collect {|x| x << "!"}
+  return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length <= 4
+    return false
+  else
+    return true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  duplicat = array & cheese_types
+  if duplicat.length >= 1
+    return duplicat.join
+  else
+    return nil
+  end
 end
